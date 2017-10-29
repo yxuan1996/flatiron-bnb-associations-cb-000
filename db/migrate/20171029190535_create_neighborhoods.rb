d6f5461483b04599aa101f3d8f61257a117314b4
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-29 22:04:07 +0000

    automatically backed up by learn

--- db/migrate/20171029190535_create_neighborhoods.rb
+++ db/migrate/20171029190535_create_neighborhoods.rb
@@ -1,8 +1,8 @@
 class CreateNeighborhoods < ActiveRecord::Migration
   def change
     create_table :neighborhoods do |t|
-      t.string :name
-      t.integer :city_id
+      t.string :name, null: false
+      t.integer :city_id, null: false
 
       t.timestamps null: false
     end
