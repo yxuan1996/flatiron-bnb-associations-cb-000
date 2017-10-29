f34076c488c84c49d11ba9f27f4a63787627b3fb
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-29 22:06:08 +0000

    automatically backed up by learn

--- db/migrate/20171029191105_create_reviews.rb
+++ db/migrate/20171029191105_create_reviews.rb
@@ -1,7 +1,10 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.integer :rating
+      t.integer :guest_id,       null: false
+      t.integer :rating,         null: false
+      t.integer :reservation_id, null: false
+
       t.string :description
 
       t.timestamps null: false
